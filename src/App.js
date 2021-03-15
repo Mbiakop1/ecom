e2b11a0a64220e9fd3a6f889950a42ac039b83aa
Mbiakop Clinton <mbiakopclinton@gmail.com>
2021-03-15 04:01:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import HomePage from "./pages/homepage/homepage.component";
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+
+
+
+const MenuItem = ({title, imageUrl, size}) => {
+    return (
+       <di className={`${size} menu-item`}>
+       <div 
+       className= 'background-image'
+       style ={{ 
+        backgroundImage: `url(${imageUrl})`
+        }}
+       >
+       
+       </div>
+        <div className="content">
+         <h1 className="title">{title.toUpperCase()}</h1>
+         <span className="subtitle">SHOP NOW</span>
+        </div>
+       </di>
+    )
+} 
+
+export default MenuItem;
