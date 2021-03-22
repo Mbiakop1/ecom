38d208e4ee563dd45c6ec7326547350e47c55b1a
Mbiakop Clinton <mbiakopclinton@gmail.com>
2021-03-22 03:40:20 +0100

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SignInWithGoogle } from "../../firebase/firebase.utils";
+import {auth, SignInWithGoogle } from "../../firebase/firebase.utils";
 
 import FormInput from '../form-input/form-input.component';
 
@@ -17,10 +17,20 @@ class SignIn extends React.Component {
     }
 
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
 
-        this.setState({ email: "", password: "" })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            
+
+            this.setState({ email: "", password: "" });
+        } catch (error) {
+            console.log(error);
+        }
+
     }
     
     handleChange = event => {
